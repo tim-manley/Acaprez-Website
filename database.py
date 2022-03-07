61cfd2390fed38f03aa7666e98dd74b6b56be98b
Tim Manley <p14tman14tman@gmail.com>
2022-03-07 05:17:25 +0000

    figured it out?

--- database.py
+++ database.py
@@ -3,13 +3,17 @@ from psycopg2 import connect
 from group import Group
 
 # Database specific variables:
-DATABASE_URL = os.environ['DATABASE_URL']
+HOST = 'ec2-3-230-238-86.compute-1.amazonaws.com'
+DATABASE = 'd8tdd1oslp407c'
+USER = 'cmjmzphzaovzef'
+PSWD='79e77741d5870f7fd84ac66ddc04c0074e407ba91b548ebd847ee076d8092600'
 
 def get_groups():
 
     groups = []
 
-    with connect(DATABASE_URL, sslmode='require') as con:
+    with connect(host=HOST, database=DATABASE, 
+                 user=USER, password=PSWD) as con:
         with con.cursor() as cur:
             cur.execute('SELECT * FROM groups;')
             
