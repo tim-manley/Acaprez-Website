321c1cbe02c67d8ac640fbe01fd801d2bf3767bd
Tim Manley <p14tman14tman@gmail.com>
2022-03-22 14:39:49 -0400

    changed optionality of dorm_room and order of arguments

--- auditionee.py
+++ auditionee.py
@@ -3,12 +3,12 @@ class Auditionee:
 
     # Will need to figure out exact implementation (what attributes are
     # necessary etc.) 
-    def __init__(self, netID, name, class_year, voice_part=None, 
-                 dorm_room=None, phone_number=None):
+    def __init__(self, netID, name, class_year, dorm_room,
+                 voice_part=None, phone_number=None):
         # Need to add error handling
         self._netID = netID
         self._name = name
         self._class_year = class_year
-        self._voice_part = voice_part
         self._dorm_room = dorm_room
+        self._voice_part = voice_part
         self._phone_number = phone_number
