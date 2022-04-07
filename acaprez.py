c8174b543e067896cdf56587f2eb23b5607bce6e
Tim Manley <p14tman14tman@gmail.com>
2022-04-07 13:38:11 -0400

    more stripping

--- acaprez.py
+++ acaprez.py
@@ -234,7 +234,7 @@ def show_group_auditions():
 
 @app.route('/createAudition', methods=['GET'])
 def createAudition():
-    _ = auth.authenticate()
+    _ = auth.authenticate().strip()
     if session.get('permissions') != 'auditionee':
         html = render_template('insufficient.html')
         response = make_response(html)
@@ -255,7 +255,7 @@ def auditioneeInfo():
         response = make_response(html)
         return response
 
-    netid = request.args.get('netID') + "\n" # newline for how cas
+    netid = request.args.get('netID')
     print("looking for this netID: ", repr(netid), file=stderr)
     auditionee = db.get_auditionee(netid)
     html = render_template('auditioneeInfo.html', auditionee=auditionee)
