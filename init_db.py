92177d2b24f3e3473898b6ff811ad8306de0bd96
ryangibbons <rjg8@princeton.edu>
2022-04-16 13:10:40 -0400

    fixes

--- init_db.py
+++ init_db.py
@@ -39,8 +39,8 @@ def create_auditionees(cur):
     cur.execute('DROP TABLE IF EXISTS auditionees;')
     cur.execute('''CREATE TABLE auditionees
                    (netID varchar(50) PRIMARY KEY,
-                    first_name varchar(50) NOT NULL,
-                    last_name varchar(50) NOT NULL,
+                    firstName varchar(50) NOT NULL,
+                    lastName varchar(50) NOT NULL,
                     classYear integer NOT NULL,
                     voicePart varchar(50),
                     dormRoom varchar(50) NOT NULL,
